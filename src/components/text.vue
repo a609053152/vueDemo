1b5f144615f360161ca15918f3889031e8b0fcb0
root <215570921@qq.com>
2020-02-04 22:07:25 +0800

    单文件组件

---
+++ src/components/text.vue
@@ -0,0 +1,28 @@
+<template>
+	<h2 class="color">66666</h2>
+</template>
+
+<script>
+export default{
+	name:'helloword',
+	props:{
+	  msg:{
+			type:String,
+			default:"textmsg"
+		}
+		
+	},
+	methods:{
+		
+	},
+	data(){
+		return{
+			key:value
+		}
+	},
+}
+	
+</script>
+.red{color:###}
+<style>
+</style>
